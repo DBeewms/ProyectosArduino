4872ee1a082498caa229fde706dc37c5e1cdf151
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-06-20 15:50:23 -0600

    Proyecto Love-o-meter

---
+++ MedidorAmor/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MedidorAmor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(12, 9);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(40, 20);
+            this.lblTemp.TabIndex = 0;
+            this.lblTemp.Text = "0.00";
+            this.lblTemp.Visible = false;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM6";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::MedidorAmor.Properties.Resources.CorazonUCA;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1023, 623);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 623);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTemp);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LOVE-O-METER | #SOMOSUCA";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTemp;
+        public System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MedidorAmor/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Autores:
+ * Diego Mora
+ * Carlos Talavera
+ * Tutor:
+ * José Durán
+ * 
+ */
+
+namespace MedidorAmor
+{
+    public partial class Form1 : Form
+    {
+        bool active = false;
+        string texto = "0.0";
+        double temperature = 0.0;
+        double baselineTemp = 23;
+
+        private void Start()
+        {
+            try
+            {
+                if (!active)
+                {
+                    serialPort1.Open();
+                    active = true;
+                }
+                else
+                {
+                    serialPort1.Close();
+                    active = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = "COM6";
+                Start();
+                timer1.Enabled = true;
+                timer1.Interval = 50;
+                timer1.Start();
+            }
+            catch (Exception ex) { }
+        }
+
+        private void getTemp()
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string[] datos = serialPort1.ReadLine().Split('-');
+                texto = datos[0];
+            }
+            catch (Exception ex) { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                lblTemp.Text = texto;
+                temperature = double.Parse(lblTemp.Text);
+                Evaluate();
+            }
+            catch (Exception ex) { }
+        }
+
+        private void Evaluate()
+        {
+            try
+            {
+                if (temperature < baselineTemp + 2)
+                {
+                    
+                    this.pictureBox1.Image = global::MedidorAmor.Properties.Resources.CorazonUCA;
+                }
+                else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4)
+                {
+                    
+                    this.pictureBox1.Image = global::MedidorAmor.Properties.Resources.abeja1;
+                }
+                else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6)
+                {
+                   
+                    this.pictureBox1.Image = global::MedidorAmor.Properties.Resources.abeja22;
+                }
+                else if (temperature >= baselineTemp + 6)
+                {
+                    
+                    this.pictureBox1.Image = global::MedidorAmor.Properties.Resources.abeja333;
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
